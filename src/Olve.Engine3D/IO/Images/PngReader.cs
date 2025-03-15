cc9a57f810a0ab452ef1cfad053c7e838b8c0606
Oliver Vea <oliver.vea@gmail.com>
2025-03-15 03:02:45 +0000

    prog: adding level loader

--- src/Olve.Engine3D/IO/Images/PngReader.cs
+++ src/Olve.Engine3D/IO/Images/PngReader.cs
@@ -1,38 +1,13 @@
-using System.Drawing;
+using BigGustave;
+using Olve.OpenRaster;
+using Olve.Utilities.Types.Results;
 
 namespace Olve.Engine3D.IO.Images;
 
-public class GrayscaleImageReader
+public class PngFileReader : IImageFileReader<Png>
 {
-    public static byte[,] ReadGrayscaleImage(string imagePath)
+    public Result<Png> ReadImage(Stream stream)
     {
-        // Load the image
-        using (Bitmap bitmap = new Bitmap(imagePath))
-        {
-            // Ensure the image is 8-bit grayscale
-            if (bitmap.PixelFormat != System.Drawing.Imaging.PixelFormat.Format8bppIndexed)
-            {
-                throw new InvalidOperationException("The image must be 8-bit grayscale.");
-            }
-
-            int width = bitmap.Width;
-            int height = bitmap.Height;
-            byte[,] pixelData = new byte[height, width];
-
-            // Loop through each pixel in the image
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    // Get the pixel color at (x, y)
-                    Color color = bitmap.GetPixel(x, y);
-
-                    // Since it's a grayscale image, we can directly use the R, G, or B value (they should all be the same)
-                    pixelData[y, x] = color.R;  // R, G, B are the same for grayscale
-                }
-            }
-
-            return pixelData;
-        }
+        return Png.Open(stream);
     }
 }
