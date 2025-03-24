a86be4a018caf24beea62cc9b2f153c4f9d60514
Oliver  Vea <oliver.vea@gmail.com>
2025-03-24 23:45:29 +0900

    ci: adding simple ci/cd pipeline

--- src/Olve.Trains.AssetPipeline/build.sh
+++ src/Olve.Trains.AssetPipeline/build.sh
@@ -3,16 +3,16 @@
 cd "$(dirname "$0")/.." || exit
 
 # Clean the temp directory
-rm -rf ./Olve.Engine3D.AssetPipeline/temp
+rm -rf ./Olve.Trains.AssetPipeline/temp
 
 # Build the docker image
-docker build -f "./Olve.Engine3D.AssetPipeline/Dockerfile" -t "olve-engine3d-asset-pipeline" .
+docker build -f "./Olve.Trains.AssetPipeline/Dockerfile" -t "olve-trains-asset-pipeline" .
 
 # Run the docker image
 docker run \
-   -v "$(pwd)/Olve.Engine3D.AssetPipeline/temp:/app/temp" \
+   -v "$(pwd)/Olve.Trains.AssetPipeline/temp:/app/temp" \
    -v "$(pwd)/Olve.Trains/assets:/app/output" \
    -v "$(pwd)/Olve.Trains/Shaders:/app/shaders:ro" \
-   --env-file "$(pwd)/Olve.Engine3D.AssetPipeline/.env" \
-   olve-engine3d-asset-pipeline \
+   --env-file "$(pwd)/Olve.Trains.AssetPipeline/.env" \
+   olve-trains-asset-pipeline \
    .
