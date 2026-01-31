7ad2e447adca237383aeeb1b1c3ff38531956b94
Oliver Vea <oliver.vea@gmail.com>
2026-01-31 11:50:07 +0900

    feat: align and justify support for layouting

--- src/Olve.Engine3D/GUI/Styling/GuiElementStyling.cs
+++ src/Olve.Engine3D/GUI/Styling/GuiElementStyling.cs
@@ -4,7 +4,7 @@ namespace Olve.Engine3D.GUI.Styling;
 
 public class GuiElementStyling<T>  where T : GuiElement
 {
-    //public Action<T>? On
+    public Action<T>? Setup { get; init; }
 
     public Action<T>? OnHoverEnter { get; init; }
     public Action<T>? OnHoverExit { get; init; }
--- src/Olve.Trains/Scenes/UI/GUI/Styling.cs
+++ src/Olve.Trains/Scenes/UI/GUI/Styling.cs
@@ -1,4 +1,5 @@
 ﻿using Olve.Engine3D.GUI.Elements;
+using Olve.Engine3D.GUI.Layout;
 using Olve.Engine3D.GUI.Styling;
 
 namespace Olve.Trains.Scenes.UI.GUI;
@@ -7,15 +8,22 @@ public static class Styles
 {
     public static readonly GuiElementStyling<Box> MenuButtonStyle = new()
     {
+        Setup = b =>
+        {
+            b.BackgroundColor = new ValueTuple<float, float, float, float>(0.2f, 0.2f, 0.2f, 0.3f);
+            b.AspectRatio = 1;
+            b.Justify = Justify.Center;
+            b.Align = Align.Center;
+            b.BorderWidth = 1f;
+            b.BorderRadius = 6f;
+        },
         OnHoverEnter = b =>
         {
             b.BorderColor = new ValueTuple<float, float, float, float>(1f, 1f, 1f, 1f);
-            b.BorderWidth = 1f;
         },
         OnHoverExit = b =>
         {
             b.BorderColor = new ValueTuple<float, float, float, float>(0.2f, 0.2f, 0.2f, 0.3f);
-            b.BorderWidth = 0.5f;
         }
     };
 }
