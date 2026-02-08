aab0314436beef41d31f14f0bae1efc347ab7d3c
Oliver Vea <oliver.vea@gmail.com>
2026-02-08 12:10:12 +0900

    fix: fixing CI/CD

--- src/Olve.Trains.AssetPipeline/build.sh
+++ src/Olve.Trains.AssetPipeline/build.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")/.." || exit
+cd "$(dirname "$0")/../.." || exit
 
 # Clean the temp directory
-rm -rf ./Olve.Trains.AssetPipeline/temp
+rm -rf ./src/Olve.Trains.AssetPipeline/temp
 
 # Build the docker image
-docker build -f "./Olve.Trains.AssetPipeline/Dockerfile" -t "olve-trains-asset-pipeline" .
+docker build -f "./src/Olve.Trains.AssetPipeline/Dockerfile" -t "olve-trains-asset-pipeline" .
 
 # Run the docker image
 docker run \
-   -v "$(pwd)/Olve.Trains.AssetPipeline/temp:/app/temp" \
-   -v "$(pwd)/Olve.Trains/assets:/app/output" \
-   -v "$(pwd)/Olve.Trains/Shaders:/app/shaders:ro" \
-   --env-file "$(pwd)/Olve.Trains.AssetPipeline/.env" \
+   -v "$(pwd)/src/Olve.Trains.AssetPipeline/temp:/app/temp" \
+   -v "$(pwd)/src/Olve.Trains/assets:/app/output" \
+   -v "$(pwd)/src/Olve.Trains/resources/shaders:/app/shaders:ro" \
+   --env-file "$(pwd)/src/Olve.Trains.AssetPipeline/.env" \
    olve-trains-asset-pipeline \
    . "$@"
