2437af24b2bd4d27a9583812be54861b6acf3c49
Oliver Vea <oliver.vea@gmail.com>
2026-02-08 11:37:33 +0900

    chore: package upgrades

--- src/Olve.Engine3D/Rendering/Entities/TerrainData.cs
+++ src/Olve.Engine3D/Rendering/Entities/TerrainData.cs
@@ -1,11 +1,11 @@
 using MemoryPack;
-using StrictId;
+using Olve.Utilities.Ids;
 
 namespace Olve.Engine3D.Rendering.Entities;
 
 [MemoryPackable]
 public partial class TerrainData
 {
-    public Id<TerrainData> Id { get; init; } = Id<TerrainData>.NewId();
+    public Id<TerrainData> Id { get; init; } = Olve.Utilities.Ids.Id.New<TerrainData>();
     public required HeightmapData Heightmap { get; set; }
 }
